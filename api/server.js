991a6f94ab6feecc8b06eb84435d4b32cd22b395
Megan Jeffcoat <jeffcoat.megan@gmail.com>
2019-03-10 12:18:36 -0500

    some file structure.....basic setup, supject to change

---
+++ api/server.js
@@ -0,0 +1,13 @@
+const express = require("express");
+
+const configureMiddleware = require("../config/middleware.js");
+const configureAuth = require("../auth/authRoute.js");
+
+const apiRouter = require('./apiRouter.js');
+const server = express();
+
+configureMiddleware(server);
+configureAuth(server);
+server.use("/", apiRouter);
+
+module.exports = server;
