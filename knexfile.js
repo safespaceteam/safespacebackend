e7c0cce6a49328e57b9f8bbe2f47b4a8b29d4031
Megan Jeffcoat <jeffcoat.megan@gmail.com>
2019-03-10 23:20:31 -0500

    added pg for production and updated knexfile.js

--- knexfile.js
+++ knexfile.js
@@ -1,4 +1,12 @@
 // Update with your config settings.
+const localPgConnection = {
+  host: 'localhost',
+  database: 'dev',
+  user: 'meg',
+  password: 'secret!',
+};
+const prodDbConnection = process.env.DATABASE_URL || localPgConnection;
+
 
 module.exports = {
   development: {
@@ -20,6 +28,16 @@ module.exports = {
       filename: './data/test.db3',
     },
     useNullAsDefault: true,
+    migrations: {
+      directory: './data/migrations',
+    },
+    seeds: {
+      directory: './data/seeds',
+    },
+  },
+  production: {
+    client: 'pg',
+    connection: prodDbConnection, // an object or a string
     migrations: {
       directory: './data/migrations',
     },
