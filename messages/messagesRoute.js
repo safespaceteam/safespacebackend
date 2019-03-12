3c1549a69a5355cb1a8d49c1a5bc8ce37c40b1d4
Megan Jeffcoat <jeffcoat.megan@gmail.com>
2019-03-11 22:11:54 -0500

    trying to narrow down error

--- messages/messagesRoute.js
+++ messages/messagesRoute.js
@@ -10,76 +10,76 @@ const errorHandler = (res, code, message, error) => {
   };
 
 
-// GET :ID
-router.get('/messages/:id', (req, res) => {
-    const id = req.params.id;
+// // GET :ID
+// router.get('/messages/:id', (req, res) => {
+//     const id = req.params.id;
   
-    db('messages')
-      .where({ id })
-      .first()
-      .then(message => {
-        if (message) {
-          db('users')
-            .where({ id })
-            .then(user => {
-              res.status(200).json(message);
-            });
-        } else {
-          res.status(400).json({
-            message: 'Could not find the message you are looking for.',
-            error
-          });
-        }
-      })
-      .catch(error => {
-        errorHandler(res, 500, 'There was an error getting the message.', error);
-      });
-  });
+//     db('messages')
+//       .where({ id })
+//       .first()
+//       .then(message => {
+//         if (message) {
+//           db('users')
+//             .where({ id })
+//             .then(user => {
+//               res.status(200).json(message);
+//             });
+//         } else {
+//           res.status(400).json({
+//             message: 'Could not find the message you are looking for.',
+//             error
+//           });
+//         }
+//       })
+//       .catch(error => {
+//         errorHandler(res, 500, 'There was an error getting the message.', error);
+//       });
+//   });
 
-// POST
-router.post('/messages', (req, res) => {
-    const newMessage = req.body;
+// // POST
+// router.post('/messages', (req, res) => {
+//     const newMessage = req.body;
   
-    db('messages')
-      .insert(newMessage)
-      .then(ids => {
-        res.status(201).json(ids);
-      })
-      .catch(error => {
-        errorHandler(res, 500, 'There was an error adding the message.', error);
-      });
-  });
+//     db('messages')
+//       .insert(newMessage)
+//       .then(ids => {
+//         res.status(201).json(ids);
+//       })
+//       .catch(error => {
+//         errorHandler(res, 500, 'There was an error adding the message.', error);
+//       });
+//   });
 
-  // DELETE
-router.delete('/messages/:id', (req, res) => {
-    const id = req.params.id;
+//   // DELETE
+// router.delete('/messages/:id', (req, res) => {
+//     const id = req.params.id;
   
-    db('messages')
-      .where({ id })
-      .del()
-      .then(count => {
-        res.status(200).json({ count });
-      })
-      .catch(error => {
-        errorHandler(res, 500, 'There was an error deleting the message', error);
-      });
-  });
+//     db('messages')
+//       .where({ id })
+//       .del()
+//       .then(count => {
+//         res.status(200).json({ count });
+//       })
+//       .catch(error => {
+//         errorHandler(res, 500, 'There was an error deleting the message', error);
+//       });
+//   });
   
-  // PUT
-  router.put('/messages/:id', (req, res) => {
-    const id = req.params.id;
-    const update = req.body;
+//   // PUT
+//   router.put('/messages/:id', (req, res) => {
+//     const id = req.params.id;
+//     const update = req.body;
   
-    db('messages')
-      .where({ id })
-      .update(update)
-      .then(count => {
-        res.status(200).json({ count });
-      })
-      .catch(error => {
-        errorHandler(res, 500, 'There was an error updating the message.', error);
-      });
-  });
+//     db('messages')
+//       .where({ id })
+//       .update(update)
+//       .then(count => {
+//         res.status(200).json({ count });
+//       })
+//       .catch(error => {
+//         errorHandler(res, 500, 'There was an error updating the message.', error);
+//       });
+//   });
 
 
 module.exports = router;
