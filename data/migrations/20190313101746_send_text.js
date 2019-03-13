e837e840dfdfba69c1c3761b0deab58b5445a585
Megan Jeffcoat <jeffcoat.megan@gmail.com>
2019-03-13 12:14:06 -0500

    message is sent successfully to the provided phone number

--- data/migrations/20190313101746_send_text.js
+++ data/migrations/20190313101746_send_text.js
@@ -1,15 +1,14 @@
 
 exports.up = function(knex, Promise) {
-    return knex.schema.createTable('send_text', messages => {
-        messages.increments();
+    return knex.schema.createTable('send_text', send_text => {
+        send_text.increments();
+
+        send_text
+        .integer('userId')
+        .references('id')
+        .inTable('users')
   
-        messages.string('phone_number', 12).notNullable();
-  
-      messages
-        .string('text_message')
-        .references('message')
-        .inTable('messages')
-        .notNullable()
+        send_text.string('phone_number', 12).notNullable();
   
     })
   };
--- twilio/twilioRouter.js
+++ twilio/twilioRouter.js
@@ -9,11 +9,39 @@ const client = require('twilio')(accountSid, authToken);
 
 const router = express.Router();
 
+const errorHandler = (res, code, message, error) => {
+    return res.status(code).json({ message, error });
+  };
+
+
+
 router.post('/send', (req, res) => {
-    client.messages
-      .create({from: twilioPhone, body: 'body', to: myPhone})
-      .then(message => console.log(message.sid));
-})
+    const id = req.body.id;
+  
+    db('messages')
+      .where({ id })
+      .then(message => {
+
+        if (message) {
+            console.log(message[0].message);
+                client.messages
+                .create({
+                    from: twilioPhone, 
+                    body: `Here is something cool about yourself: ${message[0].message}`, 
+                    to: myPhone
+                })
+                .then(message => console.log(message.sid));
+                res.status(200).json(message);
+        } else {
+          res.status(400).json({
+            message: 'Could not find the message you are looking for.'
+          });
+        }
+      })
+      .catch(error => {
+        errorHandler(res, 500, 'There was an error sending the message.', error);
+      });
+  });
 
 
 
